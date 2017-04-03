85160d245ed572354fdd3d359bf2b3369fd3b460
AuntMary <auntmary.wd@gmail.com>
2017-04-03 17:55:42 +0300

    first

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php 
+	header('HTTP/1.1 404 Not Found');
+	$pageTitle = 'Страница не найдена | Базовые понятия HTML';
+	
+		require_once ('header.php');
+?>
+		<div class="container">
+			<div class="page-header">
+				<h1>Ошибка 404 — страница не найдена</h1>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$pageTitle = 'Главная | Базовые понятия HTML';
+
+require_once ('header.php');
+
+?>
+		<div class="container">
+			<div class="page-header">
+				<h1>Сдача тестов по теме "Базовые понятия HTML"</h1>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<p class="lead">Вы можете пройти ряд тестов, для лучшего усвоения знаний по теме HTML.</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
+					
